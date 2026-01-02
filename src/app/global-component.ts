c44fe1ca1a8568ab394a8df795d76586ba355b41
Fernando Castellanos <hc16041@ues.edu.sv>
2026-01-02 16:11:25 -0600

    buscamos agregar el https

--- src/app/global-component.ts
+++ src/app/global-component.ts
@@ -1,10 +1,8 @@
 export const GlobalComponent = {
-  // Api Calling
-  API_URL: "https://api-node.themesbrand.website/",
-  headerToken: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
-
+ 
   // Auth Api
-  AUTH_API: "http://194.163.182.246:8080/api/",
+  //AUTH_API: "http://194.163.182.246:8080/api/",
   //AUTH_API: "http://23.109.152.213:8090/api/",
   //AUTH_API: "https://localhost:7117/api/",
+  AUTH_API: "https://api.194.163.182.246.nip.io/api/",
 };
